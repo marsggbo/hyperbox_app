f7309af011d3a8ef1f8bea204798ed15f1b77c3c
marsggbo <1435679023@qq.com>
2022-01-05 20:20:04 +0800

    update search bash file

--- hyperbox_app/medmnist/search_covid19.sh
+++ hyperbox_app/medmnist/search_covid19.sh
@@ -12,16 +12,21 @@ others=$3
 CUDA_VISIBLE_DEVICES=$gpu python -m hyperbox.run \
 hydra.searchpath=[file:///home/comp/18481086/code/hyperbox_app/hyperbox_app/medmnist/configs] \
 experiment=$exp \
-datamodule.img_size=[160,160] \
-datamodule.center_size=[128,128] \
+datamodule.img_size=[80,80] \
+datamodule.center_size=[64,64] \
 datamodule.slice_num=32 \
 datamodule.num_workers=3 \
-datamodule.batch_size=64 \
+datamodule.batch_size=24 \
 model/optimizer_cfg=sgd \
 model.optimizer_cfg.lr=0.025 \
 model.metric_cfg._target_=hyperbox.utils.metrics.Accuracy \
 trainer.gpus=$gpuNum \
 trainer.accelerator=dp \
+trainer.max_epochs=100 \
+++trainer.amp_backend=apex \
+++trainer.amp_level=o1 \
+callbacks.model_checkpoint.monitor='val/auc' \
+callbacks.model_checkpoint.save_top_k=1 \
 $others
 
 # experiment=gdas_ccccii.yaml \
