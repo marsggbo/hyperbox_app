f9564511347f41c4e251511a71e71034ff3bd8f3
marsggbo <1435679023@qq.com>
2022-02-13 19:04:13 +0800

    refactor mixup loss

--- hyperbox_app/medmnist/losses/mixup_loss.py
+++ hyperbox_app/medmnist/losses/mixup_loss.py
@@ -8,10 +8,11 @@ class MixupLoss(torch.nn.Module):
         self.training = False
 
     def forward(self, logits, y, *args, **kwargs):
+        # y.shape is [bs,3], each column is [real labels, permuted labels, lam weight]
         if self.training:
             loss_a = self.criterion(logits, y[:, 0].long(), *args, **kwargs)
             loss_b = self.criterion(logits, y[:, 1].long(), *args, **kwargs)
-            return ((1 - y[:, 2]) * loss_a + y[:, 2] * loss_b).mean()
+            return (y[:, 2] * loss_a + (1 - y[:, 2]) * loss_b).mean()
         else:
             return self.criterion(logits, y, *args, **kwargs)
 
