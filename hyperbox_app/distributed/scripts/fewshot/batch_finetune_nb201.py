d3e52b072c50f5495a2c705410632eb562c2e385
marsggbo <1435679023@qq>
2022-07-08 19:47:39 -0700

    update

--- hyperbox_app/distributed/scripts/fewshot/batch_finetune_nb201.py
+++ hyperbox_app/distributed/scripts/fewshot/batch_finetune_nb201.py
@@ -15,7 +15,7 @@ for idx, mask in enumerate(masks2finetune):
     gpu_id = i%num_gpus
     suffix = f"net{idx}"
     others = "ipdb_debug=False logger.wandb.offline=True trainer.strategy=null trainer.limit_val_batches=1.0"
-    cmd = f'''bash ./scripts/fewshot/fewshot_search_nb201.sh {gpu_id} {suffix} {mask} "{others}"  & sleep 10'''
+    cmd = f'''bash ./scripts/fewshot/finetune.sh {gpu_id} {suffix} {mask} "{others}"  & sleep 10'''
     i += 1
     if i % num_gpus == 0 and i > 0:
         cmd = cmd.replace('&', '')
