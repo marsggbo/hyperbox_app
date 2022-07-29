ed16b7fb5f6ae1a12120e48dd66c4dac6aa77537
marsggbo <1435679023@qq>
2022-07-28 23:06:26 -0700

    update

--- hyperbox_app/distributed/scripts/fewshot/finetune_ea.sh
+++ hyperbox_app/distributed/scripts/fewshot/finetune_ea.sh
@@ -3,19 +3,19 @@ gpuNum=${gpu//,/}
 gpuNum=${#gpuNum}
 suffix=$2
 mask=$3
+# cfgPath=$4
 others=$4
 name=finetune_${suffix}
 # name=debug
 
 echo $name
+echo $others
 
-# nb201
-# CUDA_VISIBLE_DEVICES=$gpu 
 python -m hyperbox.run \
 hydra.searchpath=[pkg://hyperbox_app.distributed.configs] \
 experiment=finetune.yaml \
 hydra.job.name=$name \
 logger.wandb.name=$name \
 trainer.gpus=$gpu \
+$others \
 model.network_cfg.mask=$mask \
-$others
